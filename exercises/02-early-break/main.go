15485d130ca4f4796bf611820497cd5d4462afe7
Hasushi <hasuprog1225@gmail.com>
2025-08-20 01:03:57 +0900

    impl: 02

--- exercises/02-early-break/main.go
+++ exercises/02-early-break/main.go
@@ -8,7 +8,12 @@ import (
 // TODO: Count(n) を実装
 func Count(n int) iter.Seq[int] {
 	return func(yield func(int) bool) {
-		// TODO
+		for i := 1; i <= n; i++ {
+			if !yield(i) {
+				fmt.Println("Count: ループを抜ける")
+				return
+			}
+		}
 	}
 }
 
