b4ca82650a7737e2c99cf49410e4fc4bc3145b7c
Hasushi <hasuprog1225@gmail.com>
2025-08-20 18:13:43 +0900

    impl: 07

--- exercises/07-error-propagation/main.go
+++ exercises/07-error-propagation/main.go
@@ -8,7 +8,20 @@ import (
 
 // TODO: Lines を実装。10行に1回エラーを出し、(zero, err) を1回だけ流して終了。
 func Lines(limit int) iter.Seq2[string, error] {
-	return nil
+	return func(yield func(string, error) bool) {
+		for i := 1; i <= limit; i++ {
+			var line string
+			var err error
+			if i%10 == 0 {
+				err = errors.New("mock read error at line 10 multiple")
+			} else {
+				line = fmt.Sprintf("line-%02d", i)
+			}
+			if !yield(line, err) {
+				return
+			}
+		}
+	}
 }
 
 func main() {
