f1ec72b2421ce528751fe81df504f7c5f9ceadee
Hasushi <hasuprog1225@gmail.com>
2025-08-20 18:44:46 +0900

    impl: 08

--- exercises/08-collectors/main.go
+++ exercises/08-collectors/main.go
@@ -6,14 +6,43 @@ import (
 )
 
 // TODO: Collect / Take / First を実装
-func Collect[T any](seq iter.Seq[T]) []T               { return nil }
-func Take[T any](seq iter.Seq[T], n int) iter.Seq[T]   { return nil }
-func First[T any](seq iter.Seq[T]) (zero T, ok bool)   { return zero, false }
+func Collect[T any](seq iter.Seq[T]) []T {
+	var result []T
+	for v := range seq {
+		result = append(result, v)
+	}
+	return result
+}
+
+func Take[T any](seq iter.Seq[T], n int) iter.Seq[T] {
+	return func(yield func(T) bool) {
+		count := 0
+		for v := range seq {
+			if !yield(v) {
+				return
+			}
+			count++
+			if count >= n {
+				return
+			}
+		}
+	}
+}
+
+func First[T any](seq iter.Seq[T]) (zero T, ok bool) {
+	next, stop := iter.Pull(seq)
+	defer stop()
+
+	v, ok := next()
+	return v, ok
+}
 
 func Count(n int) iter.Seq[int] {
 	return func(yield func(int) bool) {
 		for i := 1; i <= n; i++ {
-			if !yield(i) { return }
+			if !yield(i) {
+				return
+			}
 		}
 	}
 }
@@ -22,13 +51,23 @@ func Filter[A any](seq iter.Seq[A], pred func(A) bool) iter.Seq[A] {
 	return func(yield func(A) bool) {
 		for a := range seq {
 			if pred(a) {
-				if !yield(a) { return }
+				if !yield(a) {
+					return
+				}
 			}
 		}
 	}
 }
 
 func main() {
-	// 1..100 の偶数の先頭5個を収集
-	fmt.Println("TODO")
+	event := Filter(Count(100), func(x int) bool { return x%2 == 0 })
+	first5 := Take(event, 5)
+	fmt.Println("collect first 5 evens:", Collect(first5))
+
+	v, ok := First(event)
+	if ok {
+		fmt.Println("first even:", v)
+	} else {
+		fmt.Println("no even found")
+	}
 }
